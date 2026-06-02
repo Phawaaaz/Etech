09689307f4b5cd2bfb5b6945b6ce45ab00cf3003
YussaMoney <highshow74@gmail.com>
2026-06-02 07:46:22 +0100

    Modified: App file; added contents to the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 // import { useState } from "react";
 import "./App.css";
+import avatar from "./assets/avatar.svg";
 
 function App() {
   return (
     <>
       <section id="center">
         <div>
-          <h1>Get started</h1>
+          <Header />
         </div>
       </section>
     </>
   );
 }
 
-function Header () {
+function Header() {
   return (
-    <header>
-      <h2>
+    <header className="flex items-center justify-between">
+      <div className="bg-black text-white font-bold text-xl px-4 py-2 rounded-lg border border-gray-800">
         E-A.I
-      </h2>
-      <button>
-        <img src={}/>
+      </div>
+      <button className="h-12 w-12 rounded-full overflow-hidden border-2 border-transparent hover:border-gray-500 transition-all">
+        <img src={avatar} alt="Avatar" />
       </button>
     </header>
-  )
+  );
 }
 
 export default App;
