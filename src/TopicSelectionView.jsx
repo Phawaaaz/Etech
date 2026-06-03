dcd89a53c6649f1024de97c35518e1aad0e4f2c4
YussaMoney <highshow74@gmail.com>
2026-06-03 07:22:31 +0100

    Modified TopicSelectionView: Added form and contents

--- src/TopicSelectionView.jsx
+++ src/TopicSelectionView.jsx
@@ -1,18 +1,34 @@
+import { useState } from "react";
+
 export default function TopicSelectionView() {
+  const [topic, setTopic] = useState("");
+  const [level, setLevel] = useState("");
   return (
     <form>
-      <div>
-        <label>Topic:</label>
-        <input></input>
+      <div className="w-full bg-white text-black px-6 py-4 rounded-full flex items-center shadow-lg">
+        <label className="text-xl font-bold mr-3 select-none">Topic:</label>
+        <input
+          type="text"
+          value={topic}
+          onChange={(e) => setTopic(e.target.value)}
+          className="w-full bg-transparent text-xl font-medium focus:outline-none"
+        />
       </div>
-      <div>
-        <label>Level:</label>
-        <select>
-          <option></option>
-          <option>Beginner</option>
-          <option>Intermediate</option>
-          <option>Advanced</option>
+      <div className="w-full bg-white text-black px-6 py-4 rounded-full flex items-center relative shadow-lg">
+        <label className="text-xl font-bold mr-3 select-none">Level:</label>
+        <select
+          value={level}
+          onChange={(e) => setLevel(e.target.value)}
+          className="w-full bg-transparent text-xl font-medium focus:outline-none cursor-pointer pr-10 appearance-none"
+        >
+          <option value="" disabled hidden></option>
+          <option value="beginner">Beginner</option>
+          <option value="intermediate">Intermediate</option>
+          <option value="advanced">Advanced</option>
         </select>
+        <div className="absolute right-6 pointer-events-none text-1xl font-black">
+          ▼
+        </div>
       </div>
     </form>
   );
