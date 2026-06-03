7f7444f1c1f161b8d795abe4d869c568dc54f97a
YussaMoney <highshow74@gmail.com>
2026-06-03 08:09:56 +0100

    Modified Loading file: Added contents

---
+++ src/Loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div>
+      <h1 className="text-6xl md:text-7xl font-black tracking-widest mb-16 select-none animate-pulse">
+        E-A.I
+      </h1>
+    </div>
+  );
+}
