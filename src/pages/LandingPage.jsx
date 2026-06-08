4851dd2f7b98a83b7eaa47bd4f1a896d24bfe806
YussaMoney <highshow74@gmail.com>
2026-06-08 23:24:48 +0100

    Modified LandingPage Component: Imported and rendered HowItWorks component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import HeroSection from "../components/HeroSection";
+import HowItWorks from "../components/HowItWorks";
 import Navbar from "../components/Navbar";
 
 export default function LandingPage() {
@@ -6,6 +7,7 @@ export default function LandingPage() {
     <div className="w-full min-h-screen bg-black text-white font-sans overflow-x-hidden selection:bg-purple-600 selection:text-white">
       <Navbar />
       <HeroSection />
+      <HowItWorks />
     </div>
   );
 }
