629738b816776c3d44ff2f247c09aa79b834a6de
YussaMoney <highshow74@gmail.com>
2026-06-08 23:22:18 +0100

    Created howItWorkSteps Data file and its contents

---
+++ src/data/howItWorkSteps.js
@@ -0,0 +1,22 @@
+const steps = [
+  {
+    num: "1",
+    title: "Describe your topic",
+    desc: "State what you want to learn, your level and duration",
+    icon: "📝",
+  },
+  {
+    num: "2",
+    title: "AI creates your course",
+    desc: "Our specialized AI creates a structured lesson, examples, quizzes and projects",
+    icon: "🤖",
+  },
+  {
+    num: "3",
+    title: "Start Learning",
+    desc: "Jump right into learning.",
+    icon: "📖",
+  },
+];
+
+export default steps;
