cd5a9ce3be42e73a1189828787046a7c85a7a0d5
YussaMoney <highshow74@gmail.com>
2026-06-08 22:15:10 +0100

    Modified Landing Page file: Added class name to div

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className="w-full min-h-screen bg-black text-white font-sans overflow-x-hidden selection:bg-purple-600 selection:text-white">
       <Navbar />
       <HeroSection />
     </div>
