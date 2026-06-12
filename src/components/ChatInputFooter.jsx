48640587bd97cb1ce81db9760997985b3affc0bd
YussaMoney <highshow74@gmail.com>
2026-06-12 11:53:50 +0100

    Modified ChatInputFooter: Adjust button styling for improved hover effect

--- src/components/ChatInputFooter.jsx
+++ src/components/ChatInputFooter.jsx
@@ -3,13 +3,13 @@ import blackArrowIcon from "../assets/black-arrow.svg";
 export default function ChatInputFooter() {
   return (
     <footer className="w-full max-w-2xl mt-8 mb-4 relative z-10">
-      <div className="relative flex items-center w-full bg-[#030303] rounded-full p-1 pl-6 shadow-2xl border border-white/5">
+      <div className="relative flex items-center w-full bg-[#030303] rounded-full pl-6 shadow-2xl border border-white/5">
         <input
           type="text"
           placeholder="Reply to E-A.I"
           className="flex-1 bg-transparent text-gray-300 placeholder-gray-400 outline-none py-2 md:py-3 text-sm md:text-base font-medium"
         />
-        <button className="w-10 h-10 md:w-11 md:h-11 bg-white rounded-[2.5rem] flex items-center justify-center text-black hover:bg-gray-200 transition-colors shrink-0 ml-4 group">
+        <button className="w-10 h-10 md:w-11 md:h-11 bg-white flex items-center justify-center rounded-[2.5rem] hover:bg-gray-400 transition-colors shrink-0 ml-4 cursor-pointer group">
           <img src={blackArrowIcon} alt="Enter" className="w-6 h-6" />
         </button>
       </div>
