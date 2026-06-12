935c787522e11467db8ab1321690343dcaa36c32
YussaMoney <highshow74@gmail.com>
2026-06-12 11:29:02 +0100

    Modified ChatInputFooter: Added footer structure with input field

--- src/components/ChatInputFooter.jsx
+++ src/components/ChatInputFooter.jsx
@@ -1,3 +1,13 @@
 export default function ChatInputFooter() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className="relative flex items-center w-full bg-[#030303] rounded-full p-1 pl-6 shadow-2xl border border-white/5">
+        <input
+          type="text"
+          placeholder="Reply to E-A.I"
+          className="flex-1 bg-transparent text-gray-300 placeholder-gray-400 outline-none py-2 md:py-3 text-sm md:text-base font-medium"
+        />
+      </div>
+    </footer>
+  );
 }
