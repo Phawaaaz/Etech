ac9f30adba768bdeeaebaa1360ecdcccb044993e
YussaMoney <highshow74@gmail.com>
2026-06-12 22:52:49 +0100

    Modified HeaderWithMenu: Update MenuButton integration and improve styling

--- src/components/HeaderWithMenu.jsx
+++ src/components/HeaderWithMenu.jsx
@@ -1,26 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import avatar from "../assets/avatar.svg";
+import MenuButton from "./MenuButton";
 
 export default function HeaderWithMenu() {
   const navigate = useNavigate();
+
   return (
     <header className="flex items-center justify-between px-6 md:px-8 py-4 bg-white z-50 relative">
       <div className="flex items-center gap-5 md:gap-6">
         <div
           onClick={() => navigate("/dashboard")}
-          className="font-black text-2xl tracking-wider select-none"
+          className="font-black text-2xl tracking-wider cursor-pointer select-none"
         >
           E-A.I
         </div>
-
-        <div className="relative">
-          <button
-            //   onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="flex items-center gap-1 font-bold text-lg hover:text-gray-600 transition-colors"
-          >
-            Menu <span className="text-sm ml-2">&#9660;</span>
-          </button>
-        </div>
+        <MenuButton />
       </div>
       <button
         onClick={() => navigate("/profile")}
