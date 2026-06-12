9217a6430eb41fccbfebe8dcdcf0422be62b337c
YussaMoney <highshow74@gmail.com>
2026-06-12 11:22:02 +0100

    Modified BackButton component: Add styling and alt attribute to image

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -2,8 +2,11 @@ import arrowLeft from "../assets/arrow-left.svg";
 
 export default function BackButton({ onClick }) {
   return (
-    <button onClick={onClick}>
-      <img src={arrowLeft} />
+    <button
+      onClick={onClick}
+      className="text-white opacity-40 hover:scale-110 hover:opacity-100 cursor-pointer"
+    >
+      <img src={arrowLeft} alt="Back Step" className="w-10 h-10" />
     </button>
   );
 }
