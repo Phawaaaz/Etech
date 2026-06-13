52dea8985388b6da0742f3f12503ce8f2c632f9b
YussaMoney <highshow74@gmail.com>
2026-06-13 12:26:22 +0100

    Modified BackButton component: Added title prop for improved accessibility

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,8 +1,9 @@
 import arrowLeftIcon from "../assets/arrow-left.svg";
 
-export default function BackButton({ onClick }) {
+export default function BackButton({ onClick, title }) {
   return (
     <button
+      title={title}
       onClick={onClick}
       className="text-white opacity-40 hover:scale-110 hover:opacity-100 cursor-pointer"
     >
